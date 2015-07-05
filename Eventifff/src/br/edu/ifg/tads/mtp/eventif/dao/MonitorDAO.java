2bcb0e1f43d0eb0b1b16cdf09ca68112fc990def
matheus <matheusparte2@gmail.com>
2015-07-05 18:37:31 -0300

    fazer monitor parcial

--- Eventifff/src/br/edu/ifg/tads/mtp/eventif/dao/MonitorDAO.java
+++ Eventifff/src/br/edu/ifg/tads/mtp/eventif/dao/MonitorDAO.java
@@ -13,106 +13,171 @@ import br.edu.ifg.tads.mtp.eventif.bd.ConnectionFactory;
 
 public class MonitorDAO {
 
-	public void adicionaMonitor(int idMonitor, int idAtividade) {     //revisar isso pois não tem lógica
+	public void adicionaMonitor(int idMonitor, int idAtividade) {
 		
-		String sql = "insert into monitor(idAluno, senha) values(?,(select senha from aluno where(? = idAluno)));";
-		
+		String verifica = "select idMonitor from monitor where(idAluno=?);";
+
 		Connection con = null;
-		try{
-			con= new ConnectionFactory().getConnection();
-			PreparedStatement stmt = con.prepareStatement(sql);
-			stmt.setInt(1, idMonitor);
-			stmt.setInt(2, idMonitor);
+
+		try {
+			con = new ConnectionFactory().getConnection();
+			PreparedStatement stmt1 = con.prepareStatement(verifica);
+
+			stmt1.setInt(1, idMonitor);
+
+			ResultSet result = stmt1.executeQuery();
+
+			if (result.next()) {
+				JOptionPane
+				.showMessageDialog(null,
+						"Aluno já é um Monitor!");
+			}else{
+				
+				String verifica2 = "select horaInicio, horaEncerramento from atividade inner join alunoAtividade on atividade.idAtividade=alunoAtividade.idAtividade where(alunoAtividade.idAluno = ?)" +
+								"intersect " +
+								"select horaInicio, horaEncerramento from atividade where(idAtividade=?);";
+				try {
+					con = new ConnectionFactory().getConnection();
+					PreparedStatement stmt2 = con.prepareStatement(verifica2);
+					stmt2.setInt(1, idMonitor);
+					stmt2.setInt(2, idAtividade);
+
+					ResultSet result2 = stmt2.executeQuery();
+					if (result2.next()) {
+						JOptionPane
+						.showMessageDialog(null,
+								"Aluno Já cadastrado em uma Atividade que Sobrepoe!");
+					} else {
+						String sql = "insert into monitor(idAluno, senha) values(?,(select senha from aluno where(? = idAluno)));";
+
+						try {
+							con = new ConnectionFactory().getConnection();
+							PreparedStatement stmt = con.prepareStatement(sql);
+							stmt.setInt(1, idMonitor);
+							stmt.setInt(2, idMonitor);
+
+							stmt.execute();
+
+							adicionaMonitorAtividade(idMonitor, idAtividade);
+						} catch (SQLException e) {
+							JOptionPane.showMessageDialog(
+									null,
+									"Não foi possível inserir Monitor. "
+											+ e.getMessage());
+						} finally {
+							try {
+								con.close();
+							} catch (SQLException e) {
+								JOptionPane.showMessageDialog(null,
+										"Impossível fechar conexão");
+							}
+						}
+					}
+				} catch (SQLException e) {
+					JOptionPane.showMessageDialog(
+							null,
+							"Não foi possível inserir Monitor. "
+									+ e.getMessage());
+				}
+				
+			}
 			
-			stmt.execute();
-			adicionaMonitorAtividade(idMonitor, idAtividade);
-		}catch(SQLException e){
-			JOptionPane.showMessageDialog(null, "Não foi possível inserir Monitor. "+e.getMessage());
-		}finally{
-			try{
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null,
+					"Não foi possível inserir Monitor. " + e.getMessage());
+		} finally {
+			try {
 				con.close();
-			}catch(SQLException e){
-				JOptionPane.showMessageDialog(null, "Impossível fechar conexão");
+			} catch (SQLException e) {
+				JOptionPane
+						.showMessageDialog(null, "Impossível fechar conexão");
 			}
 		}
-		
+
 	}
-	
-	public void adicionaMonitorAtividade(int idMonitor, int idAtividade ){
+
+	public void adicionaMonitorAtividade(int idMonitor, int idAtividade) {
 
 		String sql = "insert into monitorAtividade(idMonitor, idAtividade) values(?,?);";
-		
+
 		Connection con = null;
-		try{
-			con= new ConnectionFactory().getConnection();
+		try {
+			con = new ConnectionFactory().getConnection();
 			PreparedStatement stmt = con.prepareStatement(sql);
 			stmt.setInt(1, idMonitor);
 			stmt.setInt(2, idAtividade);
-			
+
 			stmt.execute();
-			JOptionPane.showMessageDialog(null, "Monitor Cadastrado com Sucesso! ");
-		}catch(SQLException e){
+			JOptionPane.showMessageDialog(null,
+					"Monitor Cadastrado com Sucesso! ");
+		} catch (SQLException e) {
 			JOptionPane.showMessageDialog(null, "Monitor Não Cadastrado! ");
-			JOptionPane.showMessageDialog(null, "Não foi possível inserir MonitorAtividade. "+e.getMessage());
-		}finally{
-			try{
+			JOptionPane.showMessageDialog(
+					null,
+					"Não foi possível inserir MonitorAtividade. "
+							+ e.getMessage());
+		} finally {
+			try {
 				con.close();
-			}catch(SQLException e){
-				JOptionPane.showMessageDialog(null, "Impossível fechar conexão");
+			} catch (SQLException e) {
+				JOptionPane
+						.showMessageDialog(null, "Impossível fechar conexão");
 			}
 		}
 	}
-	
-	
-	public void entradaAluno(int idAluno, int idAtividade){
-		
+
+	public void entradaAluno(int idAluno, int idAtividade) {
+
 		String sql = "insert into PresencaAtividade(horaCheckin, horaCheckout, idAtividade, idAluno) values(?,?,?,?);";
-		
+
 		Connection con = null;
-		try{
+		try {
 			con = new ConnectionFactory().getConnection();
 			PreparedStatement stmt = con.prepareStatement(sql);
-			
+
 			stmt.setTimestamp(1, (Timestamp) Calendar.getInstance().getTime());
 			stmt.setTimestamp(2, null);
 			stmt.setInt(3, idAtividade);
 			stmt.setInt(4, idAluno);
-			
+
 			stmt.execute();
-		}catch(SQLException e){
-			JOptionPane.showMessageDialog(null, "Não foi possível Fazer Checkin ! "+e.getMessage());
-		}finally{
-			try{
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null,
+					"Não foi possível Fazer Checkin ! " + e.getMessage());
+		} finally {
+			try {
 				con.close();
-			}catch(SQLException e){
-				JOptionPane.showMessageDialog(null, "Impossível fechar conexão! "+ e.getMessage());
+			} catch (SQLException e) {
+				JOptionPane.showMessageDialog(null,
+						"Impossível fechar conexão! " + e.getMessage());
 			}
 		}
 	}
-	
-	public void saidaAluno(int idAluno, int idAtividade){
+
+	public void saidaAluno(int idAluno, int idAtividade) {
 		String sql = "insert into PresencaAtividade(horaCheckin, horaCheckout, idAtividade, idAluno) values(?,?,?,?);";
 		Connection con = null;
-		try{
+		try {
 			con = new ConnectionFactory().getConnection();
 			PreparedStatement stmt = con.prepareStatement(sql);
-			
+
 			stmt.setTimestamp(1, null);
 			stmt.setTimestamp(2, (Timestamp) Calendar.getInstance().getTime());
 			stmt.setInt(3, idAtividade);
 			stmt.setInt(4, idAluno);
-			
+
 			stmt.execute();
-		}catch(SQLException e){
-			JOptionPane.showMessageDialog(null, "Não foi possível Fazer Checkin ! "+e.getMessage());
-		}finally{
-			try{
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null,
+					"Não foi possível Fazer Checkin ! " + e.getMessage());
+		} finally {
+			try {
 				con.close();
-			}catch(SQLException e){
-				JOptionPane.showMessageDialog(null, "Impossível fechar conexão! "+ e.getMessage());
+			} catch (SQLException e) {
+				JOptionPane.showMessageDialog(null,
+						"Impossível fechar conexão! " + e.getMessage());
 			}
 		}
 	}
-	
-	
+
 }
